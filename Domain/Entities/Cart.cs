6e5e5fa7ae93c0a0539654d6fd83906b2721b206
Ezio Xiang <1094708529@qq.com>
2017-02-25 15:00:11 +0800

    完成第八章

--- Domain/Entities/Cart.cs
+++ Domain/Entities/Cart.cs
@@ -6,16 +6,14 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    class Cart
+    public class Cart
     {
         private List<CartLine> lineCollection = new List<CartLine>();
 
-        private void AddItem(Product product, int quantity)
+        public void AddItem(Product product, int quantity)
         {
             CartLine line = lineCollection.
-                Where(p => p.Product.ProductID == product.ProductID).
-                FirstOrDefault();
-
+                FirstOrDefault(p => p.Product.ProductID == product.ProductID);
             if (line==null)
             {
                 lineCollection.Add(new CartLine{Product = product,Quantity = quantity});
